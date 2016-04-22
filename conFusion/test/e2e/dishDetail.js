78fff3df2647008e13efd02b1476003ad33f43ea
Varun85jobs <varun85.jobs@gmail.com>
2016-04-22 07:04:22 +0100

    Adding e2e test to test the comment form.

--- conFusion/test/e2e/dishDetail.js
+++ conFusion/test/e2e/dishDetail.js
@@ -36,4 +36,35 @@ describe('menu 0 item', function () {
         expect(author.getText()).toContain('25 Cent');
 
     });
+
+
+    it('Submit button should be enabled', function(){
+
+        element(by.model("newComment.author")).sendKeys('TestAuthor');
+
+        var checkBox = element.all(by.model("newComment.rating")).get(2);
+
+        browser.actions().mouseMove(checkBox).click(); // This is so that the checkbox becomes available for clicking
+
+        element(by.model("newComment.comment")).sendKeys('Test comments');
+
+        expect(element(by.css('button[type="submit"]')).isEnabled()).toBeTruthy();
+
+        /*expect(element.all(by.repeater('comment in dish.comments'))
+            .count()).toEqual(7);*/
+
+    });
+
+
+    it('Submit button should be disabled', function(){
+
+        var checkBox = element.all(by.model("newComment.rating")).get(2);
+
+        browser.actions().mouseMove(checkBox).click(); // This is so that the checkbox becomes available for clicking
+
+        element(by.model("newComment.comment")).sendKeys('Test comments');
+
+        expect(element(by.css('button[type="submit"]')).isEnabled()).toBeFalsy();
+
+    });
 });
